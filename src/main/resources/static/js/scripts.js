bf63f9be759405837bb84aa3264b4c7b46d9344c
SSH1van <akaunt654321@gmail.com>
2025-01-30 16:11:49 +0300

    Refactor: index.html, scripts.js, styles.css

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,257 +1,169 @@
-// Ссылка на объект графика
 let chart;
-let temp_element;
-const apply_button = document.getElementById('apply-button');
+let tempElement = null;
+const applyButton = document.getElementById('apply-button');
+const progressBar = document.getElementById("loading-bar");
+const progressContainer = document.getElementById("progress-container");
+const filterInput = document.getElementById("filterInput");
+const table = document.getElementById("product-table").getElementsByTagName("tbody")[0];
+const header = document.getElementById("selected-table");
 
+// Отображает таблицу товаров для выбранной категории
 function showTable(element) {
-    apply_button.disabled = false;
-    apply_button.classList.add('enabled');
-    temp_element = element;
+    if (element === null) return;
 
+    applyButton.disabled = false;
+    tempElement = element;
+
+    // Установка заголовка и очистка старых данных
     const category = element.getAttribute("data-category");
-    const table = document.getElementById("product-table").getElementsByTagName("tbody")[0];
-    const progressBar = document.getElementById("loading-bar");
-    const progressContainer = document.getElementById("progress-container");
-    const selectedOption = document.querySelector('input[name="diffOption"]:checked');
-    const filterInput = document.getElementById("filterInput");
-
-    // Получение значений слайдеров
-    const currentPriceSlider = document.querySelectorAll("#sliders div:nth-child(1) input[type='range']");
-    const priceDiffSlider = document.querySelectorAll("#sliders div:nth-child(2) input[type='range']");
-
-    let minCurrentPrice = parseFloat(currentPriceSlider[0].value);
-    let maxCurrentPrice = parseFloat(currentPriceSlider[1].value);
-    if (minCurrentPrice > maxCurrentPrice) {
-        [minCurrentPrice, maxCurrentPrice] = [maxCurrentPrice, minCurrentPrice];
-    }
-
-    let minPriceDiff = parseFloat(priceDiffSlider[0].value);
-    let maxPriceDiff = parseFloat(priceDiffSlider[1].value);
-    if (minPriceDiff > maxPriceDiff) {
-        [minPriceDiff, maxPriceDiff] = [maxPriceDiff, minPriceDiff];
-    }
-
-    // Очистка старых данных
-    table.innerHTML = "";
-
-    // Установка заголовка
-    const header = document.getElementById("selected-table");
     header.textContent = category;
+    table.innerHTML = "";
 
     // Получение данных
     const categoryData = data[category];
-
-    if (!categoryData || typeof categoryData !== "object") {
-        console.error(`Данные для категории "${category}" не найдены.`);
-        return;
-    }
-
     const rows = Object.entries(categoryData);
-    const updateStep = Math.ceil(rows.length * 0.01);
 
-    // Показать прогресс
+    setupProgressBar(rows.length);
+    processRows(rows)
+}
+
+// Настраивает прогресс-бар
+function setupProgressBar(total) {
     progressContainer.style.display = "flex";
-    progressBar.max = rows.length;
+    progressBar.max = total;
     progressBar.value = 0;
+}
 
+// Обрабатывает строки данных в батчах
+function processRows(rows) {
+    const batchSize = Math.ceil(rows.length * 0.05); // 5% за раз
     let htmlRows = "";
 
+    const filters = getFilters();
     let minPrice = Infinity, maxPrice = -Infinity;
     let minDiff = Infinity, maxDiff = -Infinity;
 
-    // Функция обработки строк с задержкой каждые 5%
-    function processRows(startIndex) {
-        const filterText = filterInput.value.toLowerCase();
-
-        for (let i = startIndex; i < rows.length; i++) {
-            const [link, valueList] = rows[i];
-            let priceText = "Нет данных";
-            let diffText = "Нет данных";
-            let percentText = "Нет данных";
-            let isRowValid = true;
-
-            if (valueList.length > 0) {
-                const latestPrice = valueList[valueList.length - 1].price;
-                const initialPrice = valueList[0].price;
-                let penultimatePrice;
-
-                if (valueList.length !== 1) {
-                    penultimatePrice = valueList[valueList.length - 2].price;
-                } else {
-                    penultimatePrice = latestPrice;
-                }
-
-                priceText = latestPrice;
-
-                if (selectedOption && selectedOption.value.includes("start-last")) {
-                    diffText = initialPrice - latestPrice;
-                    percentText = (diffText * 100) / initialPrice;
-                    percentText = parseFloat(percentText.toFixed(1));
-                } else {
-                    diffText = penultimatePrice - latestPrice;
-                    percentText = (diffText * 100) / penultimatePrice;
-                    percentText = parseFloat(percentText.toFixed(1));
-                }
-
-                minPrice = Math.min(minPrice, latestPrice);
-                maxPrice = Math.max(maxPrice, latestPrice);
-                minDiff = Math.min(minDiff, diffText);
-                maxDiff = Math.max(maxDiff, diffText);
-
-                if (
-                    (latestPrice < minCurrentPrice || latestPrice > maxCurrentPrice) || // Фильтр по текущей цене
-                    (diffText < minPriceDiff || diffText > maxPriceDiff) || // Фильтр по разнице цен
-                    (filterText && !link.toLowerCase().includes(filterText))
-                ) {
-                    isRowValid = false;
-                }
-            }
-
-            if (isRowValid) {
-                htmlRows += `
-                    <tr>
-                        <td><a href="${link}" target="_blank">${link}</a></td>
-                        <td>${priceText}</td>
-                        <td>${diffText}</td>
-                        <td>${percentText}</td>
-                    </tr>
-                `;
-            }
-
-            // Обновляем прогресс и делаем задержку каждые updateStep строк
-            if ((i + 1) % updateStep === 0 || i === rows.length - 1) {
-                progressBar.value = i + 1;
-
-                // Если не конец, ставим задержку и продолжаем обработку
-                if (i < rows.length - 1) {
-                    setTimeout(() => processRows(i + 1), 0);
-                } else {
-                    // Если обработка завершена, обновляем таблицу, слайдеры и скрываем прогресс обработки
-                    updateSliders(minPrice, maxPrice, minDiff, maxDiff);
-                    table.innerHTML = htmlRows;
-                    progressContainer.style.display = "none";
-                }
-                return;
-            }
-        }
-    }
-
-    // Запускаем обработку с первой строки
-    processRows(0);
-}
-
-apply_button.addEventListener('click', function () {
-    showTable(temp_element);
-});
-
-function updateSliders(minPrice, maxPrice, minDiff, maxDiff) {
-    // Функция для обновления диапазона и значений слайдеров
-    function updateSliderRange(sliders, min, max) {
-        if (sliders.length === 2) {
-            sliders.forEach(slider => {
-                slider.min = min;
-                slider.max = max;
+    function processBatch(startIndex) {
+        for (let i = startIndex; i < Math.min(startIndex + batchSize, rows.length); i++) {
+            const rowHtml = processRow(rows[i], filters, (price, diff) => {
+                minPrice = Math.min(minPrice, price);
+                maxPrice = Math.max(maxPrice, price);
+                minDiff = Math.min(minDiff, diff);
+                maxDiff = Math.max(maxDiff, diff);
             });
-            sliders[0].oninput();
+
+            if (rowHtml) htmlRows += rowHtml;
         }
-    }
 
-    // Обновляем диапазон "Текущая цена"
-    const currentPriceSliders = document.querySelectorAll("#sliders > div:nth-child(1) input[type='range']");
-    updateSliderRange(currentPriceSliders, minPrice, maxPrice);
+        progressBar.value = Math.min(startIndex + batchSize, rows.length);
 
-    // Обновляем диапазон "Разница цен"
-    const priceDiffSliders = document.querySelectorAll("#sliders > div:nth-child(2) input[type='range']");
-    updateSliderRange(priceDiffSliders, minDiff, maxDiff);
-}
-
-function updateDiff() {
-    const radioButtons = document.querySelectorAll('input[name="diffOption"]');
-
-    radioButtons.forEach((radioButton) => {
-        if (radioButton.checked) {
-            radioButton.setAttribute('checked', 'checked');
+        if (startIndex + batchSize < rows.length) {
+            requestAnimationFrame(() => processBatch(startIndex + batchSize));
         } else {
-            radioButton.removeAttribute('checked');
+            updateSliders(minPrice, maxPrice, minDiff, maxDiff);
+            table.innerHTML = htmlRows;
+            progressContainer.style.display = "none";
         }
-    });
-
-    showTable(temp_element);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const tableBody = document.querySelector('#product-table tbody');
-
-    // Проверяем, что `data` доступно
-    if (typeof data !== 'object') {
-        console.error('Данные для продуктов не найдены');
-        return;
     }
 
-    // Добавляем обработчик событий на таблицу
-    tableBody.addEventListener('click', (event) => {
-        const selectedTableId = document.getElementById('selected-table').textContent.trim();
+    processBatch(0);
+}
 
-        // Проверяем существование таблицы и данных для этой таблицы
-        if (!data[selectedTableId]) {
-            console.error(`Данные для таблицы "${selectedTableId}" не найдены`);
-            return;
-        }
+// Обрабатывает одну строку и возвращает HTML, если она проходит фильтр
+function processRow([link, valueList], filters, updateMinMax) {
+    if (!valueList.length) return "";
 
-        const row = event.target.closest('tr');
-        if (row) {
-            // Получаем продукт из первой ячейки строки
-            const product = row.cells[0].textContent.trim();
-            if (data[selectedTableId][product]) {
-                showChart(selectedTableId, product); // Отображаем график для выбранного продукта
-            } else {
-                console.error(`Данные для продукта "${product}" не найдены`);
-            }
-        }
-    });
-});
+    const { minCurrentPrice, maxCurrentPrice, minPriceDiff, maxPriceDiff, diffType, filterText } = filters;
+    const latestPrice = valueList.at(-1).price;
+    const initialPrice = valueList[0].price;
+    const penultimatePrice = valueList.length > 1 ? valueList.at(-2).price : latestPrice;
 
-// Функция для сортировки таблицы
+    let diffText = diffType === "start-last" ? initialPrice - latestPrice : penultimatePrice - latestPrice;
+    let percentText = ((diffText * 100) / (diffType === "start-last" ? initialPrice : penultimatePrice)).toFixed(1);
+
+    updateMinMax(latestPrice, diffText);
+
+    if (latestPrice < minCurrentPrice || latestPrice > maxCurrentPrice ||
+        diffText < minPriceDiff || diffText > maxPriceDiff ||
+        (filterText && !link.toLowerCase().includes(filterText))) {
+        return "";
+    }
+
+    return `<tr>
+                <td><a href="${link}" target="_blank">${link}</a></td>
+                <td>${latestPrice}</td>
+                <td>${diffText}</td>
+                <td>${percentText}</td>
+            </tr>`;
+}
+
+// Получает текущие значения фильтров
+function getFilters() {
+    const selectedOption = document.querySelector('input[name="diffOption"]:checked');
+    const diffType = selectedOption?.value.includes("start-last") ? "start-last" : "prev-last";
+
+    const [minCurrentPrice, maxCurrentPrice] = getSliderRange("#sliders div:nth-child(1) input[type='range']");
+    const [minPriceDiff, maxPriceDiff] = getSliderRange("#sliders div:nth-child(2) input[type='range']");
+    const filterText = filterInput.value.toLowerCase();
+
+    return { minCurrentPrice, maxCurrentPrice, minPriceDiff, maxPriceDiff, diffType, filterText };
+}
+
+// Возвращает минимальное и максимальное значение ползунков
+function getSliderRange(selector) {
+    const sliders = document.querySelectorAll(selector);
+    let min = parseFloat(sliders[0].value);
+    let max = parseFloat(sliders[1].value);
+    return min > max ? [max, min] : [min, max];
+}
+
+// Обновляет значения ползунков на основе минимальных и максимальных значений цен и разницы цен
+function updateSliders(minPrice, maxPrice, minDiff, maxDiff) {
+    updateSliderRange("#sliders > div:nth-child(1) input[type='range']", minPrice, maxPrice);
+    updateSliderRange("#sliders > div:nth-child(2) input[type='range']", minDiff, maxDiff);
+}
+
+// Устанавливает минимальные и максимальные значения для ползунков и обновляет их визуальное состояние
+function updateSliderRange(selector, min, max) {
+    const sliders = document.querySelectorAll(selector);
+    if (sliders.length === 2) {
+        sliders.forEach(slider => {
+            slider.min = min;
+            slider.max = max;
+        });
+        sliders[0].oninput();
+    }
+}
+
+// Применяет выбранный тип сравнения цен
+function updateDiff() {
+    document.querySelector('input[name="diffOption"]:checked')?.setAttribute('checked', 'checked');
+    showTable(tempElement);
+}
+
+// Сортирует таблицу по указанному столбцу
 function sortTable(columnIndex) {
     const table = document.getElementById("product-table");
     const tbody = table.querySelector("tbody");
     const rows = Array.from(tbody.rows);
 
-    // Определяем порядок сортировки: asc (по умолчанию) или desc
+    // Определяем порядок сортировки. Для последней цены от меньшего к большему, для остальных наоборот
     let isAscending = columnIndex === 1;
 
-    // Сортируем строки
     rows.sort((rowA, rowB) => {
-        const cellA = rowA.cells[columnIndex].textContent.trim();
-        const cellB = rowB.cells[columnIndex].textContent.trim();
-
-        // Проверяем, являются ли значения числами
-        const valueA = parseFloat(cellA);
-        const valueB = parseFloat(cellB);
-
-        if (!isNaN(valueA) && !isNaN(valueB)) {
-            // Если оба значения числа, сравниваем их как числа
-            return isAscending ? valueA - valueB : valueB - valueA;
-        } else {
-            // Если значения строки, сравниваем их как строки
-            return isAscending ? cellA.localeCompare(cellB) : cellB.localeCompare(cellA);
-        }
+        const valueA = parseFloat(rowA.cells[columnIndex].textContent) || rowA.cells[columnIndex].textContent.trim();
+        const valueB = parseFloat(rowB.cells[columnIndex].textContent) || rowB.cells[columnIndex].textContent.trim();
+        return isAscending ? (valueA > valueB ? 1 : -1) : (valueA < valueB ? 1 : -1);
     });
 
-    // Перестраиваем таблицу
     tbody.innerHTML = "";
     rows.forEach(row => tbody.appendChild(row));
 }
 
-// Добавляем обработчики событий для заголовков таблицы
-document.addEventListener("DOMContentLoaded", () => {
-    const headers = document.querySelectorAll("#product-table thead th");
-
-    headers.forEach((header, index) => {
-        header.addEventListener("click", () => sortTable(index));
-    });
-});
-
+/************************************************
+ *              ФУНКЦИОНАЛ ГРАФИКА              *
+ ************************************************/
+// Преобразует строку даты
 function formatDate(input) {
     const [datePart, timePart] = input.split('_');
     const [day, month, year] = datePart.split('-');
@@ -262,25 +174,24 @@ function formatDate(input) {
     return date.toISOString().slice(0, 19);
 }
 
+// Отображает график изменения цены для выбранного товара
 function showChart(selectedTableId, product) {
-    // Получаем данные для выбранного товара
     const productData = data[selectedTableId][product] || [];
 
     const dataset = {
-        label: "product",
+        label: "Цена",
         data: productData.map(point => ({
-            x: new Date(formatDate(point.timestamp)), // Временная метка
-            y: point.price // Цена
+            x: new Date(formatDate(point.timestamp)),
+            y: point.price
         })),
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 2,
         fill: false
     };
 
-    // Если график уже существует, обновляем его
     const ctx = document.getElementById('priceChart').getContext('2d');
     if (chart) {
-        chart.data.datasets = [dataset];  // Обновляем данные
+        chart.data.datasets = [dataset];
         chart.update();
     } else {
         // Создаём график, если его нет
@@ -302,3 +213,42 @@ function showChart(selectedTableId, product) {
         });
     }
 }
+
+/************************************************
+ *            СОБЫТИЯ ДЛЯ ЭЛЕМЕНТОВ             *
+ ************************************************/
+// Повторно загружает таблицу при нажатии на кнопку "Применить фильтры".
+applyButton.addEventListener('click', () => showTable(tempElement));
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Сортировка при клике на заголовок таблицы
+    document.querySelectorAll("#product-table thead th").forEach((header, index) => {
+        header.addEventListener("click", () => sortTable(index));
+    });
+
+    // Вызов функции отрисовки графика для товара
+    document.querySelector('#product-table tbody').addEventListener('click', event => {
+        const row = event.target.closest('tr');
+        if (!row) return;
+
+        const product = row.cells[0].textContent.trim();
+        const selectedTableId = header.textContent.trim();
+
+        if (data[selectedTableId]?.[product]) {
+            showChart(selectedTableId, product);
+        } else {
+            console.error(`Данные для продукта "${product}" не найдены`);
+        }
+    });
+
+    // Нажатие на категорию товара
+    document.getElementById('category-list').addEventListener('click', (event) => {
+        const liElement = event.target.closest('li');
+        showTable(liElement);
+    });
+
+    // Изменение режима отображения разницы цен
+    document.querySelectorAll('#price-diff-options input[name="diffOption"]').forEach(radio => {
+        radio.addEventListener('change', updateDiff);
+    });
+});
