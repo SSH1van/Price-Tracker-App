7e331f3b7368acbd6ed0c8149a589aca5f784795
SSH1van <akaunt654321@gmail.com>
2025-02-07 11:28:54 +0300

    Update: styles

--- src/main/java/com/ivan/pricetrackerapp/SecurityConfig.java
+++ src/main/java/com/ivan/pricetrackerapp/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/login", "/css/login.css", "/img/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
