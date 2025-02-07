1075d85d796cc402d714a9642067d6305f8000be
SSH1van <akaunt654321@gmail.com>
2025-02-07 11:40:58 +0300

    Fix: Username and password are removed from the code

--- src/main/java/com/ivan/pricetrackerapp/SecurityConfig.java
+++ src/main/java/com/ivan/pricetrackerapp/SecurityConfig.java
@@ -1,21 +1,46 @@
 package com.ivan.pricetrackerapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${app.security.username}")
+    private String username;
+
+    @Value("${app.security.password}")
+    private String password;
+
+    @Bean
+    // Безопасное хеширование
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username(username)
+                .password(passwordEncoder().encode(password))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    // Хеширование паролей с BCrypt
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Настройки для отображаемых страниц до аутентификации, выхода из аккаунта
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -35,22 +60,5 @@ public class SecurityConfig {
                 );
 
         return http.build();
-    }
-
-    @Bean
-    // Безопасное хеширование
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("1")
-                .password(passwordEncoder().encode("1"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    // Хеширование паролей с BCrypt
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
