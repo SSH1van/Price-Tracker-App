7040dad862271580cb9d7b1cba73b5b10fed02ec
SSH1van <akaunt654321@gmail.com>
2025-02-28 18:15:33 +0300

    Add: SQL file for the database

---
+++ products.sql
@@ -0,0 +1,19 @@
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE products (
+    id BIGSERIAL PRIMARY KEY,
+    url TEXT NOT NULL UNIQUE,
+    category_id INT NOT NULL REFERENCES categories(id) ON DELETE CASCADE
+);
+
+CREATE TABLE product_prices (
+    id BIGSERIAL PRIMARY KEY,
+    product_id BIGINT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    date TIMESTAMP(0) NOT NULL,
+    price INT NOT NULL
+);
+
+CREATE INDEX idx_product_prices_date ON product_prices (date);
